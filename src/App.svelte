034fcbc9f1052474017d81c6d650e1f1ac1df60b
notAperson535 <notAperson939@gmail.com>
2023-01-27 10:04:16 -0500

    more design changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,80 @@
 <script>
-	export let name;
+  let tabsandiframes = [1, 2];
+
+  let nextid = 3;
+
+  function go(url) {
+    let activeIframe = document.querySelector("iframe.active");
+
+    activeIframe.style.display = "block";
+    activeIframe.src = "/go/" + url;
+  }
+
+  let tabs = [];
+  let iframes = [];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>Abyss is functioning with Svelte!!</p>
-</main>
-<style>
+<div id="topbar">
+  <div id="tabs">
+    {#each tabsandiframes as tabandiframe, index (tabandiframe)}
+      <button
+        class="tab"
+        id={"tab" + tabandiframe}
+        on:click={() => {
+          let tab = document.querySelector("#tab" + tabandiframe);
+          let iframe = document.getElementById(tabandiframe);
+          tabs.forEach((elmnt) => {
+            elmnt.classList = "tab";
+          });
+          iframes.forEach((elmnt) => {
+            elmnt.classList = "";
+            elmnt.style.display = "none";
+          });
+          tab.classList.add("active");
+          iframe.classList.add("active");
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+          if (iframe.src !== "") {
+            iframe.style.display = "block";
+          } else {
+            iframe.style.display = "none";
+          }
+        }}
+        bind:this={tabs[index]}
+      >
+        <img
+          class="favicon"
+          alt="new tab icon"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABEElEQVR4nO2ZwWrCQBRFj0KC/oe/2NpV0U9w1ZX+Qb+i3Xdf/ZFK4ElgCsNAMDOaPIP3wNvlJfdyJ1ncgBBCiIFYAlvgBDSAjTxNePYmaMkW/+0g2jqm1bLIMbB9ANGWzHuOgVO0+AJUV65fAZ/JA/fAnHIq4DW632/Ocnzm65478yD6nibq5J3oTSwihxnwkewfbjRhJVpKDXQlUTJn4MfDQFcS95jRDPwnsQP+pmrgVqrkKzQ5A+lXaJIGWmTAG9MRcsaUgDOmBJwxJeCMKQFnTAk4Y0rAGXvKBJqCYmsI6tJiK64W104mauCttFrcDNDpjFruLh+sXv/KrdcJC63ro+MPjmPQkC1eCCEEfbgAuJ68dbQb+kgAAAAASUVORK5CYII="
+        />
+        <p>New tab</p>
+      </button>
+    {/each}
+  </div>
+  <div id="settingsandnewtab">
+    <button>
+      <i class="fa-solid fa-gear" />
+    </button>
+    <button
+      on:click={() => {
+        let newnextid = nextid;
+        let newtabsandiframes = [...tabsandiframes, newnextid];
+        tabsandiframes = newtabsandiframes;
+        nextid = nextid + 1;
+        function checkIfTabExists() {
+          if (document.querySelector("#tab" + newnextid) == null) {
+            window.setTimeout(checkIfTabExists, 50);
+          } else {
+            document.querySelector("#tab" + newnextid).click();
+          }
+        }
+        checkIfTabExists();
+      }}
+    >
+      <i class="fa-solid fa-plus" />
+    </button>
+  </div>
+</div>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#each tabsandiframes as tabandiframe, index (tabandiframe)}
+  <iframe id={tabandiframe} title="iframe" src="" bind:this={iframes[index]} />
+{/each}
