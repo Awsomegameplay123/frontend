a1eda48f6798fd23ad56f69c67ee908d3aa79d1a
notAperson535 <notAperson939@gmail.com>
2023-03-14 18:05:25 -0400

    abyss beta finished

--- public/sw-register.js
+++
@@ -1,7 +0,0 @@
-import { proxyApi, prefix } from "./aero/config.js";
-
-import ProxyManager from "./sdk/ProxyManager.js";
-
-const proxyManager = new ProxyManager();
-
-proxyManager.add("/sw.js", prefix);
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   let tabsandiframes = [];
 
+  let inputurl;
   let nextid = 1;
+  let tabOrder = [];
 
   function isUrl(val = "") {
     if (
@@ -18,9 +20,20 @@
     else if (!(url.startsWith("https://") || url.startsWith("http://")))
       url = "https://" + url;
     let activeIframe = document.querySelector("iframe.active");
+    url = __uv$config.encodeUrl(url);
 
     activeIframe.style.display = "block";
-    activeIframe.src = "/go/" + url;
+    activeIframe.src = "/service/" + url;
+
+    inputurl = __uv$config.decodeUrl(url);
+  }
+
+  function closeTabAndIframe(id) {
+    tabsandiframes.splice(tabsandiframes.indexOf(id), 1);
+    tabsandiframes = tabsandiframes;
+    tabOrder.splice(tabOrder.indexOf(id), 1);
+    tabOrder = tabOrder;
+    document.getElementById("tab" + tabOrder.slice(-1)[0]).click();
   }
 
   let tabs = [];
@@ -35,6 +48,11 @@
           class="tab"
           id={"tab" + tabandiframe}
           on:click={() => {
+            if (tabOrder.indexOf(tabandiframe) > -1) {
+              tabOrder.splice(tabOrder.indexOf(tabandiframe), 1);
+            }
+            tabOrder[tabOrder.length] = tabandiframe;
+
             let tab = document.querySelector("#tab" + tabandiframe);
             let iframe = document.getElementById(tabandiframe);
             tabs.forEach((elmnt) => {
@@ -52,6 +70,8 @@
             } else {
               iframe.style.display = "none";
             }
+            let url = iframe.src.split(__uv$config.prefix)[1];
+            inputurl = __uv$config.decodeUrl(url);
           }}
           bind:this={tabs[index]}
         >
@@ -61,6 +81,15 @@
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABEElEQVR4nO2ZwWrCQBRFj0KC/oe/2NpV0U9w1ZX+Qb+i3Xdf/ZFK4ElgCsNAMDOaPIP3wNvlJfdyJ1ncgBBCiIFYAlvgBDSAjTxNePYmaMkW/+0g2jqm1bLIMbB9ANGWzHuOgVO0+AJUV65fAZ/JA/fAnHIq4DW632/Ocnzm65478yD6nibq5J3oTSwihxnwkewfbjRhJVpKDXQlUTJn4MfDQFcS95jRDPwnsQP+pmrgVqrkKzQ5A+lXaJIGWmTAG9MRcsaUgDOmBJwxJeCMKQFnTAk4Y0rAGXvKBJqCYmsI6tJiK64W104mauCttFrcDNDpjFruLh+sXv/KrdcJC63ro+MPjmPQkC1eCCEEfbgAuJ68dbQb+kgAAAAASUVORK5CYII="
           />
           <p>New tab</p>
+          <button
+            class="tabclose"
+            on:click={(event) => {
+              event.stopPropagation();
+              closeTabAndIframe(tabandiframe);
+            }}
+          >
+            <i class="fa-regular fa-circle-xmark" />
+          </button>
         </button>
       {/each}
     </div>
@@ -90,8 +119,12 @@
       </button>
     </div>
   </div>
-  <form id="urlbar">
-    <input type="text" />
+  <form on:submit|preventDefault={() => go(inputurl)} id="urlbar">
+    <input
+      bind:value={inputurl}
+      placeholder="Search or type a URL"
+      type="text"
+    />
   </form>
 </div>
 
@@ -102,5 +135,8 @@
 <svelte:window
   on:load={() => {
     document.querySelector("#newtabbutton").click();
+    window.navigator.serviceWorker.register("./sw.js", {
+      scope: __uv$config.prefix,
+    });
   }}
 />
