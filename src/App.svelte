901e439daaecb800998328e6a14fb91e6798326e
paxton! <103937192+paxtonwarin@users.noreply.github.com>
2023-03-30 22:47:07 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,42 @@
     return false;
   }
 
+  function cloak() {
+		let inFrame
+		try {
+			inFrame = window !== top
+		} catch (e) {
+			inFrame = true
+		}
+		if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+			const popup = open("about:blank", "_blank")
+			if (!popup || popup.closed) {
+				alert("Popups are disabled!");
+				var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://ssl.gstatic.com/classroom/favicon.png';
+	document.title = 'Google Classroom';
+    document.getElementsByTagName('head')[0].appendChild(link);
+			} else {
+				const doc = popup.document
+				const iframe = doc.createElement("iframe")
+				const style = iframe.style
+				const link = doc.createElement("link")
+				doc.title = "Loading..."
+				link.rel = "icon";
+				link.href = "https://ssl.gstatic.com/classroom/favicon.png";
+				iframe.src = location.href
+				style.position = "fixed"
+				style.top = style.bottom = style.left = style.right = 0
+				style.border = style.outline = "none"
+				style.width = style.height = "100%"
+				doc.body.appendChild(iframe)
+				location.replace("https://schoology.com")
+			}
+		}
+	  }
+  
   function popOut() {
     var win = window.open();
     var url = document.querySelector('iframe.active').src;
@@ -114,9 +150,9 @@
 <button on:click={() => go('https://discord.gg/goabyss')}>
         <i class="fa-brands fa-discord" />
       </button>
-       <button on:click={() => 				alert("There are currently no settings implemented! Come back soon!")     }>
-        <i class="fa-solid fa-gear" />
-       </button>
+       <button on:click={() => cloak()}>
+<i class="fa-solid fa-masks-theater"></i>
+      </button>
       <button
         id="newtabbutton"
         on:click={() => {
